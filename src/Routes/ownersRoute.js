43e08dff65e9812b130403c2c82e5f738950835d
sebastian lozano <sebaslozano708@gmail.com>
2024-12-18 20:45:45 -0500

    Added another Join to add person's pets data

--- src/Routes/ownersRoute.js
+++ src/Routes/ownersRoute.js
@@ -5,7 +5,7 @@ const { getOwners } = require("../controllers/ownersController.js");
 const ownersRouter = Router();
 
 
-ownersRouter.get("/owners", getOwners);
+ownersRouter.get("/owners/:owner_id", getOwners);
 
 
 
--- src/controllers/ownersController.js
+++ src/controllers/ownersController.js
@@ -4,6 +4,8 @@ const database = require("../config/database.js");
 
 const getOwners = async (req, res) => {
     
+    const { owner_id } = req.params;
+
     try {
         await database.connect();
         const {rows} = await database.query(`
@@ -16,12 +18,19 @@ const getOwners = async (req, res) => {
                         ARRAY['car_id', 'car'],
                         ARRAY[cars.id::text, cars.car]
                     )
-                ) AS cars
+                ) AS cars,
+                JSON_AGG(
+                    JSON_OBJECT(
+                        ARRAY['pet_id', 'pet_name', 'pet_breed'],
+                        ARRAY[pets.id::text, pet_name, breed]
+                    )
+                ) AS pets
             FROM person
             LEFT JOIN cars ON cars.person_id = person.id
+            LEFT JOIN pets ON pets.person_id = person.id
+            WHERE person.id = $1
             GROUP BY person.id 
-        `);
-        console.log("Rows :", rows);
+        `, [owner_id]);
 
         await database.end();
         res.status(200).json(rows);
